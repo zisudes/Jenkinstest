34a88dbb269a381d28bd063a6e766e6b9913d2f5
Your Name <Your Email>
2025-02-13 14:45:38 +0200

    test

---
+++ src/main/java/org/example/Cal.java
@@ -0,0 +1,23 @@
+package org.example;
+
+
+public class Cal {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public double divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Cannot divide by zero");
+        }
+        return (double) a / b;
+    }
+}
